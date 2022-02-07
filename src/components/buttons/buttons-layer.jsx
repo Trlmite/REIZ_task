896bacee1d16a70befc36eb1b603dd6dcee94c82
Trollmite <m.gytautas@gmail.com>
2022-02-07 14:37:17 +0200

    button sorting A-Z

--- src/components/buttons/buttons-layer.jsx
+++ src/components/buttons/buttons-layer.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box} from '@mui/material';
 import FilterButton from "./filter-button";
 
 const ButtonLayer = () => {
+    const[sorting, setSorting] = useState(true)
+
+    const handleSorting = (e) =>{
+        if(sorting){
+            setSorting(false)
+        } else {
+            setSorting(true)
+        }
+
+    }
 
     return(
         <Box sx={{display: "flex", justifyContent: 'space-between'}}>
@@ -16,7 +26,8 @@ const ButtonLayer = () => {
             </Box>
             <Box>
             <FilterButton
-                name="A-Z"
+                name={sorting ? "A-Z" : "Z-A"}
+                onClick={handleSorting}
                 />
             </Box>
         </Box>
--- src/components/buttons/filter-button.jsx
+++ src/components/buttons/filter-button.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Button} from "@mui/material";
 
-const FilterButton = ({name}) => {
+const FilterButton = ({name, onClick}) => {
 
     return(
         <Button 
         variant="contained" 
         sx={{bgcolor: "success.main",
         borderRadius: 8}}
+        onClick={onClick}
         >
         {name}
         </Button>
